d58f5b1cd4d6d3dd3a1d4cf8ef427348f356f5a4
rafrozen <rafrozenluoxin@gmail.com>
2021-07-12 21:16:09 +0800

    display trait for vec

--- study/vecDisplay.rs
+++ study/vecDisplay.rs
@@ -14,10 +14,38 @@ impl fmt::Display for List {
         for (count, v) in vec.iter().enumerate() {
             // 对每个元素（第一个元素除外）加上逗号。
             // 使用 `?` 或 `try!` 来返回错误。
-			dbg!(count);
-            if count%2 == 0 { write!(f, " : ")?; }
-            else {write!(f, " , ")?;}
-            write!(f, "{}", v)?;
+            write!(f, "{0} : {1}", count, v)?;
+            if count != vec.len()-1 {write!(f, ", ")?;}
+            // if count%2 == 0 { write!(f, " : ")?; }
+            // else {write!(f, " , ")?;}
+            // write!(f, "{}", v)?;
+
+        }
+
+        // 加上配对中括号，并返回一个 fmt::Result 值。
+        write!(f, "]")
+    }
+}
+
+// 不能实现 第三方 trait 第三方 类型
+// 孤儿规则
+// compile panic
+impl<T> fmt::Display for Vec<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
+
+        write!(f, "[")?;
+
+        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
+        for (count, v) in self.iter().enumerate() {
+            // 对每个元素（第一个元素除外）加上逗号。
+            // 使用 `?` 或 `try!` 来返回错误。
+            write!(f, "{0} : {1}", count, v)?;
+            if count != self.len()-1 {write!(f, ", ")?;}
+            // if count%2 == 0 { write!(f, " : ")?; }
+            // else {write!(f, " , ")?;}
+            // write!(f, "{}", v)?;
+
         }
 
         // 加上配对中括号，并返回一个 fmt::Result 值。
